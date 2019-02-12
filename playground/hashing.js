cb12087ea9657c63b86d6cf4efb85a5d86dd5ddd
Jignesh Darji <jignesh.darji@gmail.com>
2019-02-11 22:13:30 -0500

    add post user routes & token

---
+++ playground/hashing.js
@@ -0,0 +1,34 @@
+const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+var data = {
+    id:2
+}
+var token = jwt.sign(data,'test123');
+console.log(`TokenL ${token}`);
+
+var decCoded =jwt.verify(token, 'test123');
+
+console.log('decoded :', decCoded);
+// var message = "testing 123User";
+// var hasgMSg = SHA256(message).toString();
+
+// console.log(`Message : ${message}`);
+// console.log(`Hash: ${hasgMSg}`);
+
+// var data = {
+//     id:4
+// };
+
+// var token ={
+//     data,
+//     hash:SHA256(JSON.stringify(data) + 'testSomething').toString()
+// }
+
+// var resultHAsh = SHA256(JSON.stringify(token.data) + 'testSomethingj').toString();
+
+// if(resultHAsh===token.hash){
+//     console.log('data was not changed');
+// }else{
+//     console.log('Data was changed..')
+// }
