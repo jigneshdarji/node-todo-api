30cb84a0418813bac05ef5c258ff2c3a302468d3
Jignesh Darji <jignesh.darji@gmail.com>
2019-02-11 23:14:22 -0500

    auth middleware

--- server/models/user.js
+++ server/models/user.js
@@ -44,13 +44,34 @@ UserSchema.methods.generateAuthToken = function(){
     var access = 'auth';
     var token = jwt.sign({_id:user._id.toHexString(), access},'testAbc123').toString();
 
+    // console.log(`USER Token: ${token}`);
     user.tokens = user.tokens.concat([{access, token}]);
 
     return user.save().then(()=>{
         return token;
-    })
+    });
 };
 
+UserSchema.statics.findByToken = function(token){
+    var User = this;
+    var decoded;
+    try{
+        decoded = jwt.verify(token, 'testAbc123');
+    }
+    catch(e){
+        // return new Promise((resolve,reject)=>{
+        //     reject();
+        // });
+
+        return Promise.reject();
+    }
+
+    return User.findOne({
+        '_id':decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    });
+};
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
