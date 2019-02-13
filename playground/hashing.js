ffd300e97742d3511e1b026c0466167a7584fb70
Jignesh Darji <jignesh.darji@gmail.com>
2019-02-12 20:38:03 -0500

    hash password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,24 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id:2
-}
-var token = jwt.sign(data,'test123');
-console.log(`TokenL ${token}`);
 
-var decCoded =jwt.verify(token, 'test123');
+var password = '123@ser';
 
-console.log('decoded :', decCoded);
+bcrypt.genSalt(10, (err,salt)=>{
+    bcrypt.hash(password,salt,(err,hash)=>{
+        console.log(hash);
+    });
+});
+// var data = {
+//     id:2
+// }
+// var token = jwt.sign(data,'test123');
+// console.log(`TokenL ${token}`);
+
+// var decCoded =jwt.verify(token, 'test123');
+
+// console.log('decoded :', decCoded);
 // var message = "testing 123User";
 // var hasgMSg = SHA256(message).toString();
 
